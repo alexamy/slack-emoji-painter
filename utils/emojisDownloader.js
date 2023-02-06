1c0a566bd08628d97086735721fa213c22002cfc
Alexander Chaschin <ac@happynumbers.com>
2023-02-07 00:40:08 +0300

    add download file scripts

--- utils/emojisDownloader.js
+++ utils/emojisDownloader.js
@@ -1,4 +1,4 @@
-const result = {};
+const emojis = {};
 const allCount = parseInt(document.querySelector('.p-customize_emoji_wrapper__count').textContent);
 let lastVisited = '';
 const log = (s) => console.log('[SEP] %c%s', 'background: #2f3640; color: #00a8ff; font-weight: bold; font-size: 18px; padding: 1px', s);
@@ -9,9 +9,9 @@ while(true) {
   emojis.forEach(emoji => {
     const src = emoji.querySelector('img').src;
     const name = emoji.querySelector('.c-table_view_row_item .black').textContent.trim();
-    result[name] = src;
+    emojis[name] = src;
   });
-  log(`Emojis saved: ${Object.keys(result).length}/${allCount}...`);
+  log(`Emojis saved: ${Object.keys(emojis).length}/${allCount}...`);
 
   const last = emojis.slice(-5)[0];
   if(lastVisited === last.textContent) {
@@ -23,3 +23,9 @@ while(true) {
   last.scrollIntoView();
   await new Promise(resolve => setTimeout(resolve, 2000));
 }
+const file = new Blob([JSON.stringify(emojis, null, 2)], { type: 'text/json' });
+const a = document.createElement('a');
+a.href = URL.createObjectURL(file);
+a.download = 'emojis.json';
+document.body.appendChild(a);
+a.click();
