b199f298d55fd5c1c85628abd96ec88d02e02ecc
Alexander Chashchin <ac@happynumbers.com>
2024-02-29 16:06:51 +0100

    add store

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
+import { createStore } from 'solid-js/store';
 
 export function App() {
+  const [store, setStore] = createStore({});
+
   return (
     <>
       <h1>Vite + Solid</h1>
