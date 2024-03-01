4df91269af6f586a4a2fa4274cd13accf14257a6
Alexander Chashchin <ac@happynumbers.com>
2024-03-01 14:21:57 +0100

    Refactor: use typescript, refactor code, qol improvements (#3)

---
+++ src/App.tsx
@@ -0,0 +1,279 @@
+import "./App.css";
+import { For, createMemo, createSignal, useContext } from "solid-js";
+import { AppContext, StoreProvider } from "./store";
+
+// app
+export function App() {
+  return (
+    <StoreProvider>
+      <div class="app">
+        <CurrentEmoji />
+        <List />
+        <Buttons />
+        <FieldSize />
+        <Field />
+        <Help />
+      </div>
+    </StoreProvider>
+  );
+}
+
+// buttons
+function Buttons() {
+  const [store, { clearWith, asText, loadEmojis }] = useContext(AppContext);
+
+  function clearWithBackground() {
+    clearWith(store.bg);
+  }
+
+  function copy() {
+    console.log("Copied.");
+    const text = asText();
+    navigator.clipboard.writeText(text);
+  }
+
+  function load(file: File | undefined) {
+    if (!file) return;
+    loadEmojis(file);
+  }
+
+  return (
+    <div class="buttons">
+      <button onClick={clearWithBackground}>Clear</button>
+      <button onClick={copy}>Copy</button>
+      <button>
+        <label for="files" class="btn">
+          Load images JSON
+        </label>
+      </button>
+      <input
+        id="files"
+        type="file"
+        accept=".json"
+        style={{ visibility: "hidden" }}
+        title="Contact authorized personnel to acquire images"
+        onChange={(e) => load(e.target.files?.[0])}
+      />
+    </div>
+  );
+}
+
+// current foreground and background emojis
+function CurrentEmoji() {
+  const [store, { setStore }] = useContext(AppContext);
+
+  function switchList(e: MouseEvent) {
+    e.preventDefault();
+    setStore("isListOpened", !store.isListOpened);
+    window.scrollTo(0, 0);
+  }
+
+  function swap() {
+    setStore({ fg: store.bg, bg: store.fg });
+  }
+
+  return (
+    <div class="current-emoji">
+      Foreground:
+      <img
+        class="emoji"
+        src={store.images[store.fg]}
+        onClick={switchList}
+        onContextMenu={switchList}
+      />
+      Background:
+      <img
+        class="emoji"
+        src={store.images[store.bg]}
+        onClick={switchList}
+        onContextMenu={switchList}
+      />
+      <button onClick={swap}>Swap</button>
+    </div>
+  );
+}
+
+// field size controls
+function FieldSize() {
+  const [store, { setStore }] = useContext(AppContext);
+
+  return (
+    <div class="field-size">
+      Width:
+      <input
+        type="number"
+        class="counter"
+        value={store.width}
+        onInput={(e) => setStore("width", parseInt(e.target.value))}
+        min={1}
+      />
+      Height:
+      <input
+        type="number"
+        class="counter"
+        value={store.height}
+        onInput={(e) => setStore("height", parseInt(e.target.value))}
+        min={1}
+      />
+    </div>
+  );
+}
+
+// the field itself
+function Field() {
+  const [store, { setStore, updateCell }] = useContext(AppContext);
+
+  function onMouseDown(e: MouseEvent, row: number, col: number) {
+    e.preventDefault();
+    setStore("mouse", () => {
+      if (e.button === 0) return "left";
+      if (e.button === 2) return "right";
+      return null;
+    });
+    updateCell(row, col);
+  }
+
+  function onMouseOver(e: MouseEvent, row: number, col: number) {
+    e.preventDefault();
+    updateCell(row, col);
+  }
+
+  function onMouseUp(e: MouseEvent) {
+    e.preventDefault();
+    setStore("mouse", null);
+  }
+
+  return (
+    <div class="field-outer" onContextMenu={(e) => e.preventDefault()}>
+      <div class="field" onMouseLeave={onMouseUp}>
+        <For each={store.field}>
+          {(emojis, row) => (
+            <div>
+              <For each={emojis}>
+                {(cell, col) => (
+                  <img
+                    class="emoji"
+                    src={store.images[cell]}
+                    onClick={(e) => e.preventDefault()}
+                    onContextMenu={(e) => e.preventDefault()}
+                    onMouseDown={(e) => onMouseDown(e, row(), col())}
+                    onMouseOver={(e) => onMouseOver(e, row(), col())}
+                    onMouseUp={(e) => onMouseUp(e)}
+                  />
+                )}
+              </For>
+            </div>
+          )}
+        </For>
+      </div>
+    </div>
+  );
+}
+
+// emojis list
+function List() {
+  const [store, { setStore }] = useContext(AppContext);
+
+  const [search, setSearch] = createSignal("");
+  const filtered = createMemo(() => {
+    if (search() === "") return store.images;
+
+    const query = search().toLowerCase();
+    const result: Record<string, string> = {};
+    for (const [name, url] of Object.entries(store.images)) {
+      if (name.includes(query)) {
+        result[name] = url;
+      }
+    }
+    return result;
+  });
+
+  function onMouseDown(e: MouseEvent, name: string) {
+    e.preventDefault();
+    if (e.button === 0) setStore("fg", name);
+    if (e.button === 2) setStore("bg", name);
+  }
+
+  return (
+    <div class="list" style={{ display: store.isListOpened ? "flex" : "none" }}>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={search()}
+        onInput={(e) => setSearch(e.target.value)}
+      />
+      <div class="emojis">
+        <For each={Object.entries(filtered())}>
+          {([name, url]) => (
+            <img
+              class="emoji"
+              src={url}
+              title={name}
+              onContextMenu={(e) => e.preventDefault()}
+              onMouseDown={(e) => onMouseDown(e, name)}
+            />
+          )}
+        </For>
+      </div>
+    </div>
+  );
+}
+
+// help messages
+function Help() {
+  return (
+    <div>
+      Drawing:
+      <ul>
+        <li>
+          Click on the field with the <b>left mouse button</b> to draw the
+          foreground emoji.
+        </li>
+        <li>
+          Click on the field with the <b>right mouse button</b> to draw the
+          background emoji.
+        </li>
+        <li>Hold the mouse button to paint like a brush.</li>
+        <li>
+          Click <b>swap</b> button to swap foreground and background emojis.
+        </li>
+        <li>
+          <i>
+            Copy emojis with the <b>copy</b> button and paste them into Slack!
+          </i>
+        </li>
+      </ul>
+      Emoji list:
+      <ul>
+        <li>
+          Click on foreground or background emoji to open emoji picker, click
+          again to hide.
+        </li>
+        <li>Click on the emoji to set foreground emoji.</li>
+        <li>Right-click on the emoji to to set background emoji.</li>
+      </ul>
+      Buttons:
+      <ul>
+        <li>
+          <b>Clear</b> button fills the entire field with the background emoji.
+        </li>
+        <li>
+          <b>Copy</b> button copies the field content as text to the clipboard.
+        </li>
+        <li>
+          <b>Load images JSON</b> button loads a JSON file with emojis.
+        </li>
+        <li>
+          The JSON should be structured as an object where each entry consists
+          of a "name" key and a corresponding "url" value.
+        </li>
+      </ul>
+      Field settings:
+      <ul>
+        <li>
+          Use the <b>width</b> / <b>height</b> sliders to change the field size.
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/store.tsx
@@ -0,0 +1,211 @@
+import { createStore, produce } from "solid-js/store";
+import { onMount, createEffect, createContext, JSX } from "solid-js";
+
+export type Store = ReturnType<typeof createAppStore>;
+
+interface StoreData {
+  version: number;
+  width: number;
+  height: number;
+  field: string[][];
+  images: Record<string, string>;
+  mouse: "left" | "right" | null;
+  fg: string;
+  bg: string;
+  isListOpened: boolean;
+}
+
+export const AppContext = createContext<Store>([] as unknown as Store);
+
+export function StoreProvider(props: { children: JSX.Element }) {
+  const store = createAppStore();
+
+  return (
+    <AppContext.Provider value={store}>{props.children}</AppContext.Provider>
+  );
+}
+
+// store
+function createAppStore() {
+  const [store, setStore] = createStore<StoreData>({
+    version: 1,
+    width: 8,
+    height: 4,
+    mouse: null,
+    fg: "",
+    bg: "",
+    isListOpened: false,
+    field: [],
+    images: {
+      ":-satan-:":
+        "https://emoji.slack-edge.com/T47BK6X1U/-satan-/e40cbb4f8726fae4.jpg",
+      ":12ozmouse-buttermilk:":
+        "https://emoji.slack-edge.com/T47BK6X1U/12ozmouse-buttermilk/2e626d7ad2ff12bb.png",
+    },
+  });
+
+  function clearWith(emoji: string) {
+    setStore("field", () => {
+      const field = [];
+      for (let i = 0; i < store.height; i++) {
+        const row = Array(store.width).fill(emoji);
+        field.push(row);
+      }
+      return field;
+    });
+  }
+
+  function asText() {
+    let text = "";
+    for (const row of store.field) {
+      for (const cell of row) {
+        text += cell;
+      }
+      text += "\n";
+    }
+
+    return text;
+  }
+
+  function loadEmojis(file: File) {
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      (e) => {
+        const text = e.target?.result;
+        if (typeof text !== "string") return;
+
+        const images = validateEmojis(text);
+        if (!images) return;
+
+        setStore({ images });
+      },
+      { once: true },
+    );
+    reader.readAsText(file);
+  }
+
+  function updateCell(row: number, col: number) {
+    setStore(
+      "field",
+      produce((field) => {
+        if (!store.mouse) return;
+        const emoji = store.mouse === "left" ? store.fg : store.bg;
+        field[row][col] = emoji;
+      }),
+    );
+  }
+
+  const methods = {
+    setStore,
+    clearWith,
+    asText,
+    loadEmojis,
+    updateCell,
+  };
+
+  persistStore([store, methods]);
+  syncSelectedEmojis([store, methods]);
+  syncFieldSize([store, methods]);
+
+  return [store, methods] as const;
+}
+
+function persistStore(state: Store) {
+  const [store, { setStore }] = state;
+
+  // load store from local storage on mount if available
+  onMount(() => {
+    const raw = localStorage.getItem("store");
+    if (!raw) return;
+
+    try {
+      const data = JSON.parse(raw);
+      if (data.version !== store.version) {
+        throw new Error("Version mismatch.");
+      }
+      setStore(data);
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  });
+
+  // save store to local storage
+  createEffect(() => {
+    const data = JSON.stringify(store);
+    localStorage.setItem("store", data);
+  });
+}
+
+function syncSelectedEmojis(state: Store) {
+  const [store, { setStore }] = state;
+
+  // if new images loaded, set the current fg and bg to the first two images
+  createEffect(() => {
+    const [first, second] = Object.keys(store.images);
+
+    if (!store.images[store.fg]) {
+      setStore("fg", first);
+    }
+    if (!store.images[store.bg]) {
+      setStore("bg", second ?? first);
+    }
+  });
+}
+
+function syncFieldSize(state: Store) {
+  const [store, { setStore }] = state;
+
+  // change the height of the field
+  createEffect(() => {
+    setStore(
+      "field",
+      produce((field) => {
+        if (store.height < field.length) {
+          field.length = store.height;
+        } else if (store.height > field.length) {
+          for (let i = field.length; i < store.height; i++) {
+            const row = Array(store.width).fill(store.bg);
+            field.push(row);
+          }
+        }
+      }),
+    );
+  });
+
+  // change the width of the field
+  createEffect(() => {
+    setStore(
+      "field",
+      Array.from({ length: store.height }).map((_, i) => i),
+      produce((row) => {
+        if (store.width < row.length) {
+          row.length = store.width;
+        } else if (store.width > row.length) {
+          for (let i = row.length; i < store.width; i++) {
+            row.push(store.bg);
+          }
+        }
+      }),
+    );
+  });
+}
+
+function validateEmojis(text: string) {
+  try {
+    const images = JSON.parse(text);
+    // check what images is an object with string keys and values starting with "http"
+    if (typeof images !== "object") throw new Error("Not an object.");
+    for (const [key, value] of Object.entries(images)) {
+      if (typeof key !== "string") throw new Error("Key is not a string.");
+      if (typeof value !== "string") throw new Error("Value is not a string.");
+      if (!value.startsWith("http")) throw new Error("Value is not a URL.");
+    }
+
+    return images as Record<string, string>;
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+}
