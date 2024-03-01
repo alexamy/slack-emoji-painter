dfa251caf76ef6ef75441d9779bccf880efddbd8
Alexander Chashchin <ac@happynumbers.com>
2024-03-01 09:10:52 +0100

    rename to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import { render } from "solid-js/web";
 import { App } from "./App";
 import "./index.css";
 
-const root = document.getElementById("root");
+const root = document.getElementById("root")!;
 
 render(() => <App />, root);
